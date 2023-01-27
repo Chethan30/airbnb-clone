cfd602088a016d5884c126d122817826889a5e84
Chethan B N <47515084+Chethan30@users.noreply.github.com>
2023-01-26 21:18:27 -0600

    Cleaning Up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import NavBar from "./components/Header/NavBar";
-import Filter from "./components/Filter/Filter";
 import CardHolder from "./components/Card/CardHolder";
 
 function App() {
